bc035bd30f9ef9a0b419f083788619099dbab32d
Tran Quang Huy <huy.tran3.intern@ntq-solution.com.vn>
2021-10-04 10:57:29 +0700

    token

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { ethers } from "ethers";
 import React from 'react';
 import Web3 from 'web3';
+import Moralis from 'moralis';
+import Spinner from 'react-bootstrap/Spinner'
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { formatEther } from "@ethersproject/units";
@@ -33,9 +35,23 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
 export default function App({ account, etherBalance }) {
   const [error, setError] = useState();
   const [txs, setTxs] = useState([]);
+  const [disabled, setDisabled] = useState(false)
+  const web3 = Moralis.enable();
+  // const contract = new web3.eth.Contract(contractAbi, contractAddress);
   // const [errorAddress, setErrrAddress] = useState(false);
   const SignupSchema = yup.object().shape({
-    addr: yup.string().test(
+    addrtoken: yup.string().test(
+      'Account', 'Account not exist',
+      async (value) => {
+        let result = Web3.utils.isAddress(value)
+        if (result === false) {
+          return false
+        } else {
+          return true
+        }
+      }
+    ),
+    contractaddr:  yup.string().test(
       'Account', 'Account not exist',
       async (value) => {
         let result = Web3.utils.isAddress(value)
@@ -58,37 +74,64 @@ export default function App({ account, etherBalance }) {
     resolver: yupResolver(SignupSchema)
   });
   const onSubmit = async (data) => {
-    // const data = new FormData(e.target);
     setError();
-    // console.log(JSON.stringify(data));
-    // let result = Web3.utils.isAddress(data.addr)
-    // result ?
-      await startPayment({
-        setError,
-        setTxs,
-        ether: data.ether.toString(),
-        addr: data.addr
-      }) 
-      // : setErrrAddress(true)
+    // await startPayment({
+    //   setError,
+    //   setTxs,
+    //   ether: data.ether.toString(),
+    //   addr: data.addr
+    // })
+  };
+  const onSubmitToken = async (data) => {
+    try {
+      setDisabled(true)
+      const options = {
+        type: "erc20",
+        amount: Moralis.Units.Token(data.tokenpay.toString(), "18"),
+        receiver: data.addrtoken,
+        contractAddress: data.contractaddr
+      }
+      let result = await Moralis.transfer(options)
+      result && setDisabled(false)
+      result && toast.success("Transaction success")
+      console.log("ket qua: ", result);
+    } catch (err) {
+      toast.error(err.message);
+      setDisabled(false)
+    }
   };
   return (
-    <form action="" onSubmit={handleSubmit(onSubmit)} className="FormContainer">
-      <h3>Send ETH payment</h3>
-      <div className="InputContainer">
-        <input type="text" name="addr" placeholder="Recipient Address" {...register("addr")} required />
-        {/* {errorAddress ?
-          <div className="ErrorEther">
-            <p>Address does not exist</p>
-          </div> : <></>
-        } */}
-        {errors.addr && <p style={{ color: "red", textAlign: "left" }} >{errors.addr.message}</p>}
-        <input type="text" name="ether" placeholder="Amount in ETH" {...register("ether")} required />
-        {errors.ether && <p style={{ color: "red", textAlign: "left" }} >{errors.ether.message}</p>}
-      </div>
-      <div className="PayBtnContainer">
-        <button className="PayBtn" >Pay Now</button>
-      </div>
-      <ToastContainer />
-    </form>
+    <>
+      {/* <form action="" onSubmit={handleSubmit(onSubmit)} className="FormContainer">
+        <h3>Send ETH payment</h3>
+        <div className="InputContainer">
+          <input type="text" name="addr" placeholder="Recipient Address" {...register("addr")} />
+          {errors.addr && <p style={{ color: "red", textAlign: "left" }} >{errors.addr.message}</p>}
+          <input type="text" name="ether" placeholder="Amount in ETH" {...register("ether")} />
+          {errors.ether && <p style={{ color: "red", textAlign: "left" }} >{errors.ether.message}</p>}
+        </div>
+        <div className="PayBtnContainer">
+          <button className="PayBtn" >Pay Now</button>
+        </div>
+        <ToastContainer />
+      </form> */}
+      <form action="" onSubmit={handleSubmit(onSubmitToken)} className="FormContainer">
+        <h3>Send ERC20 Tokens payment</h3>
+        <div className="InputContainer">
+          <input type="text" name="addrtoken" placeholder="Recipient Address" {...register("addrtoken")} required />
+          {errors.addrtoken && <p style={{ color: "red", textAlign: "left" }} >{errors.addrtoken.message}</p>}
+          <input type="text" name="contractaddr" placeholder="Contract Address" {...register("contractaddr")} required />
+          {errors.contractaddr && <p style={{ color: "red", textAlign: "left" }} >{errors.contractaddr.message}</p>}
+          <input type="text" name="tokenpay" placeholder="Amount in Token" {...register("tokenpay")} required />
+          {errors.tokenpay && <p style={{ color: "red", textAlign: "left" }} >{errors.tokenpay.message}</p>}
+        </div>
+        <div className="PayBtnContainer">
+          {!disabled ? <button className="PayBtn" >Pay Now</button> : <Spinner animation="border" role="status">
+  <span className="visually-hidden">Loading...</span>
+</Spinner>}
+        </div>
+        <ToastContainer />
+      </form>
+    </>
   )
 }
